4838d4d4f65a4e8073a758c86b1ffe902aeb01fc
jasmine-ah <107646587+jasmine-ah@users.noreply.github.com>
2022-08-01 17:04:42 +0300

    Add files via upload

---
+++ loginForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labEx2{
+    public partial class loginForm : Form
+    {
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "admin" || textBox1.Text == "user") && textBox2.Text == "admin")
+            {
+
+            }
+            else {
+                MessageBox.Show(" Incorrect!!Try Again");
+                    }
+            Form1 screen=new Form1(textBox1.Text);
+            screen.Show();
+            this.Hide();
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
