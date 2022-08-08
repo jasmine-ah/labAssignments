3e8b1dbb0291fe9b85a877b803d3464008de174b
jasmine-ah <yasminip30@gmail.com>
2022-08-08 19:37:27 +0300

    updated

---
+++ Model/Class2.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labEx2.Model
+{
+    internal class Class2
+    {
+        static private List<Class2> class2=new List<Class2>();
+        public string pid{get; set;}
+          public string pname{get; set;}
+            public string amount{get; set;}
+              public string price{get; set;}
+                public string WR{get; set;}
+                  public string SR{get; set;}
+
+       public void save()
+        {
+            class2.Add(this);
+            MessageBox.Show("Query executed");
+        }           
+public static Class2 findOne(string name){
+            return class2.Find(c=> pname==name);
+        }
+
+
+        public static List<Class2> getAllProducts()
+        {
+            return class2;
+        }
+
+    
+
+    }
+}
--- Model/class1.cs
+++ Model/class1.cs
@@ -13,10 +13,11 @@ namespace labEx2.Model
         public bool radiobutton1 { get; set; }
         static private List<Class1> class1 = new List<Class1>();
         public string Id { get; set; }
+        public bool Name{get; set;}
         public string ObjectName { get; set; }
-
-
-
+        public bool Male{get; set;}
+        public bool Female{get; set;}
+        public bool phone{get; set;}
         public string inventoryNumber { get; set; }
 
         public string price { get; set; }
@@ -27,6 +28,9 @@ namespace labEx2.Model
         {
             class1.Add(this);
             MessageBox.Show("Query executed");
+        }
+        public static Class1 findOne(string name){
+            return class1.Find(c=> c.name==name);
         }
 
 
